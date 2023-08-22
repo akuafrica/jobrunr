b07438a21035ee0f14e7e6a148b1ad403a169f7f
Umoh Bassey-Duke <umohb@aku.africa>
2023-08-22 16:12:53 +0200

    Removing unused files and saving changes

--- framework-support/jobrunr-quarkus-extension/tests/src/main/java/org/jobrunr/quarkus/it/JobRunrApp.java
+++
@@ -1,9 +0,0 @@
-package org.jobrunr.quarkus.it;
-
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/jobrunr")
-public class JobRunrApp extends Application {
-}
